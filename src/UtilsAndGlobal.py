915f61bfe36c3ca370161a19f97e3503f2449aa4
Jidgdoi <Jidgdoi@users.noreply.github.com>
2016-01-15 17:00:34 +0100

    Create UtilsAndGlobal.py

---
+++ src/UtilsAndGlobal.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+# Cyril Fournier
+# 15/01/2016
+
+
+######################
+# GLOBAL VARIABLES
+######################
+# --- Cell type
+CellTypePath = 11760
+CellTypeGlass = 17977
+CellTypeWall = 12701
+# --- Cell item
+CellItemNone = 19725
+CellItemPacman = 18419
+CellItemPoint = 14759
+CellItemPower = 10799
+CellItemGhost = 13012
