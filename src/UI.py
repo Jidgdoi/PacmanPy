cefc19d48676696b988f704f95460770134f32b8
Cyril Fournier <cyril.fournier@dijon.inra.fr>
2016-04-28 17:35:47 +0200

    Implementing key catching with WX.

--- src/UI.py
+++ src/UI.py
@@ -8,14 +8,42 @@ import tty,termios # for getch
 import threading
 import time
 import Queue
+import wx
 
 import UtilsAndGlobal as UAG
+
+class UICatcher(wx.Frame):
+	"""
+	Windows catching user's keyboard event.
+	"""
+	def __init__(self):
+		wx.Frame.__init__(self, None, wx.ID_ANY, "Pacman game", size=(0,0))
+		
+		# Add a panel so it looks the correct on all platforms
+		panel = wx.Panel(self, wx.ID_ANY)
+		btn = wx.TextCtrl(panel, value="")
+		btn.SetFocus()
+		
+		btn.Bind(wx.EVT_CHAR, self.onCharEvent)
+
+	def onCharEvent(self, event):
+		keycode = event.GetKeyCode()
+		controlDown = event.CmdDown()
+		altDown = event.AltDown()
+		shiftDown = event.ShiftDown()
+		
+		print keycode
+		if keycode == wx.WXK_UP: print "Up arrow"
+		elif keycode == wx.WXK_DOWN: print "Down arrow"
+		elif keycode == wx.WXK_RIGHT: print "Right arrow"
+		elif keycode == wx.WXK_LEFT: print "Left arrow"
+		event.Skip()
 
 # ===========================
 #    ===   Class UI   ===
 # ===========================
 
-class UI(threading.Thread):
+class UI(threading.Thread, wx.Frame):
 	"""
 	Object controlling the interaction with the user: User Interface.
 	This class use one thread.
@@ -32,6 +60,32 @@ class UI(threading.Thread):
 		self.queue = queue
 		self.queueLock = queueLock
 		self.delay = delay
+		
+		# ========
+		# Frame init
+		# ========
+		wx.Frame.__init__(self, None, wx.ID_ANY, "Pacman game", size=(0,0))
+		
+		# Add a panel so it looks the correct on all platforms
+		panel = wx.Panel(self, wx.ID_ANY)
+		btn = wx.TextCtrl(panel, value="")
+		btn.SetFocus()
+		
+		btn.Bind(wx.EVT_CHAR, self.onCharEvent)
+
+	def onCharEvent(self, event):
+		keycode = event.GetKeyCode()
+		controlDown = event.CmdDown()
+		altDown = event.AltDown()
+		shiftDown = event.ShiftDown()
+		
+		print keycode
+		if keycode == wx.WXK_UP: print "Up arrow"; self.sendQuery('\x1b[A')
+		elif keycode == wx.WXK_DOWN: print "Down arrow"; self.sendQuery('\x1b[B')
+		elif keycode == wx.WXK_RIGHT: print "Right arrow"; self.sendQuery('\x1b[C')
+		elif keycode == wx.WXK_LEFT: print "Left arrow"; self.sendQuery('\x1b[D')
+		else: print "Destroy"
+		event.Skip()
 
 	# ----------------------------------
 	# --- Private functions
@@ -62,6 +116,13 @@ class UI(threading.Thread):
 		elif key == '\x1b[C': return UAG.MovementRight
 		elif key == '\x1b[D': return UAG.MovementLeft
 		return False
+
+	def sendQuery(self, direction):
+		query = [UAG.CellCharacterPacman, direction]
+		self.queueLock.acquire()
+		self.queue.put(query)
+		self.queueLock.release()
+		time.sleep(self.delay)
 
 	def run(self):
 		mvt = True
--- src/keyCatch.py
+++ src/keyCatch.py
@@ -17,12 +17,22 @@ class MyForm(wx.Frame):
 		controlDown = event.CmdDown()
 		altDown = event.AltDown()
 		shiftDown = event.ShiftDown()
- 		
+		
+		print "Control event: %s" %controlDown
+		print "Alt event: %s" %altDown
+		print "Shift event: %s" %shiftDown
+		
 		print keycode
 		if keycode == wx.WXK_SPACE:
 			print "you pressed the spacebar!"
 		elif controlDown and altDown:
 			print keycode
+		elif keycode == wx.WXK_LEFT:
+			print "Left arrow"
+		elif keycode == wx.WXK_UP:
+			print "Up arrow"
+		elif keycode == 97:
+			self.Destroy()
 		event.Skip()
  
  
@@ -31,4 +41,6 @@ if __name__ == "__main__":
 	app = wx.PySimpleApp()
 	frame = MyForm()
 	frame.Show()
+	print "Before"
 	app.MainLoop()
+	print "End"
